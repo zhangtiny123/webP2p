278df08bc5544f5de7100cd73ef4f9f2d7fb9dcb
Tao Zhang <taozhang@CNtaozhang.local>
2014-12-19 15:29:30 +0800

    tzhang - first commit

---
+++ core/src/main/resources/db/seed/init-user.sql
@@ -0,0 +1,6 @@
+insert into USER ( NAME, PASSWORD, EMAIL, AGE) values ('Tom', 'password','tom@abc.com', 16);
+insert into USER ( NAME, PASSWORD, EMAIL, AGE) values ('Jerry', 'password','jerry@abc.com', 17);
+insert into USER ( NAME, PASSWORD, EMAIL, AGE) values ('Red', 'password','red@abc.com', 18);
+insert into USER ( NAME, PASSWORD, EMAIL, AGE) values ('Marry', 'password','marry@abc.com', 19);
+insert into USER ( NAME, PASSWORD, EMAIL, AGE) values ('James', 'password','james@abc.com', 20);
+insert into USER ( NAME, PASSWORD, EMAIL, AGE) values ('Emma', 'password','emma@abc.com', 21);
