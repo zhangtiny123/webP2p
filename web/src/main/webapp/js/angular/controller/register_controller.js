66c296911218ca3327a3a2205da3fd3e1475717e
zhangtiny123 <tzhang920314@gmail.com>
2014-12-30 15:46:35 +0800

    tzhang - add password and email verify

--- web/src/main/webapp/js/angular/controller/register_controller.js
+++ web/src/main/webapp/js/angular/controller/register_controller.js
@@ -4,8 +4,21 @@ angular.module('webP2p')
         $scope.user = {};
 
         $scope.to_complete_information = function() {
-            localStorage["user_base_info"] = JSON.stringify($scope.user);
-            $location.path("/register_complete")
+            if ($scope.user.password != $scope.user.re_password) {
+                alert("twice typed password are not same, retype please!")
+            }
+            else if (!is_mail_legal($scope.user.email)) {
+                alert("illegal email, retype please!")
+            }
+            else {
+                localStorage["user_base_info"] = JSON.stringify($scope.user);
+                $location.path("/register_complete");
+            }
+        }
+
+        function is_mail_legal(email) {
+            var email_reg =  /^([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/;
+            return email_reg.test(email);
         }
 
         $scope.go_to_welcome_page = function() {
